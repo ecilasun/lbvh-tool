1af598d6107826ab0d4b512fba7bbff2241d7dfa
Engin Cilasun <ecilasun@me.com>
2022-04-09 14:22:48 -0700

    Initial submit

---
+++ bvh8tool.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int _argc, char** _argv)
+{
+	if (_argc<=1)
+	{
+		printf("BVH8 Tool v0.0\nUsage:\nbvh8tool inputfile outputfile\n");
+		return 0;
+	}
+
+	// TODO:
+
+	return 0;
+}
